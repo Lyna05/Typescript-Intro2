af8a7f7c7b496b03306daacb0b8af8c27971b92d
Lyna05 <lyne_armelle@hotmail.com>
2024-03-21 14:17:14 +0100

    Adding typescript and homework.

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("Heute ist Donnerstag");
+console.log("tsc -w app.ts");
+// Variablen deklarieren
+var trainer = 'Lucas';
+var tag = 'string';
+// var ist böse
+// var getränk = 'Kaffee'
+tag = 'Freitag';
+// tag = 4
+console.log(tag);
+function verdoppeln(zahl) {
+    return zahl * 2;
+}
+console.log(verdoppeln(50));
---
+++ app.ts
@@ -0,0 +1,20 @@
+console.log("Heute ist Donnerstag")
+console.log("tsc -w app.ts")
+
+// Variablen deklarieren
+
+const trainer = 'Lucas';
+let tag = 'string'
+
+// var ist böse
+// var getränk = 'Kaffee'
+
+tag = 'Freitag'
+// tag = 4
+console.log(tag)
+
+function verdoppeln(zahl: number): number {
+    return zahl * 2
+}
+
+console.log(verdoppeln(50))
