db51530befddf1783e0bf7d0589a240c6f9b1462
Lyna05 <lyne_armelle@hotmail.com>
2024-03-22 16:55:45 +0100

    Adding Homework.

--- friday/HA22.03.24/HA3.js
+++ friday/HA22.03.24/HA3.js
@@ -1,41 +1,27 @@
-
-// Typdefinition für die Inhalte der Obstschüssel
-type Obstschüssel = {
-  schüsselGröße: string;
-  schüsselFarbe: string;
-  ausgewählteObstsorten: string[];
-};
-
 // Zugriff auf die HTML-Elemente
-const schüsselGrößeInput = document.getElementById('schüsselGröße')! as HTMLSelectElement;
-const schüsselFarbeInput = document.getElementById('schüsselFarbe')! as HTMLSelectElement;
-const obstInputs = document.getElementsByName('obst');
-
+var schüsselGrößeInput = document.getElementById('schüsselGröße');
+var schüsselFarbeInput = document.getElementById('schüsselFarbe');
+var obstInputs = document.getElementsByName('obst');
 // Eventlistener für das Absenden des Formulars
-const form = document.getElementById('form')! as HTMLFormElement;
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  // Schüsselgröße auslesen
-  const ausgewählteSchüsselGröße = schüsselGrößeInput.value;
-
-  // Schüsselgröße und -farbe speichern
-  const obstschüssel: Obstschüssel = {
-    schüsselGröße: ausgewählteSchüsselGröße,
-    schüsselFarbe: schüsselFarbeInput.value,
-    ausgewählteObstsorten: []
-  };
-
-  // Ausgewählte Obstsorten auslesen und in das Obstschüssel-Objekt speichern
-  for (let i = 0; i < obstInputs.length; i++) {
-    const aktuellesObst = obstInputs[i] as HTMLInputElement;
-    if (aktuellesObst.checked) {
-      obstschüssel.ausgewählteObstsorten.push(aktuellesObst.value);
+var form = document.getElementById('form');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Schüsselgröße auslesen
+    var ausgewählteSchüsselGröße = schüsselGrößeInput.value;
+    // Schüsselgröße und -farbe speichern
+    var obstschüssel = {
+        schüsselGröße: ausgewählteSchüsselGröße,
+        schüsselFarbe: schüsselFarbeInput.value,
+        ausgewählteObstsorten: []
+    };
+    // Ausgewählte Obstsorten auslesen und in das Obstschüssel-Objekt speichern
+    for (var i = 0; i < obstInputs.length; i++) {
+        var aktuellesObst = obstInputs[i];
+        if (aktuellesObst.checked) {
+            obstschüssel.ausgewählteObstsorten.push(aktuellesObst.value);
+        }
     }
-  }
-
-  // Ausgabe in der Konsole
-  console.log("Ausgewählte Schüsselgröße:", obstschüssel.schüsselGröße);
-  console.log("Ausgewählte Obstsorten:", obstschüssel.ausgewählteObstsorten);
+    // Ausgabe in der Konsole
+    console.log("Ausgewählte Schüsselgröße:", obstschüssel.schüsselGröße);
+    console.log("Ausgewählte Obstsorten:", obstschüssel.ausgewählteObstsorten);
 });
